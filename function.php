ecd31169352ec06e2b97db4063215b7d24c088e3
Marcos Palladino <marcospalladino@gmail.com>
2020-08-07 06:45:32 -0300

    primeito commit

---
+++ function.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Twenty Nineteen functions and definitions
+ *
+ * @link http://www.torneseumprogramador.com.br
+ *
+ * @package WordPress
+ * @subpackage desafio21diasWordpress
+ * @since Desafio 21 dias 1.0
+ */
+?>
